122641e1288f9c2db40640f512ad9dba5189c0f3
Hanjey <zhunxun@gmail.com>
2016-11-02 10:47:49 +0800

    change pre shell

--- pre_vtpm.sh
+++ pre_vtpm.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 echo '----------------------------------'
-echo 'create VTPM store file'
-mkdir /var/lib/libvirt/vtpm
+if [ -f /var/lib/libvirt/vtpmfile ]
+then 
+echo 'vtpmfile is done!'
+else
+echo 'create vtpmfile directory!'
+touch /var/lib/libvirt/vtpmfile
+fi
 echo '----------------------------------'
+echo 'install policy module'
+semanage -i allowTPM.pp
 echo 'change filecontext'
-semanage fcontext  -m -s system_u -t virt_image_t "/var/lib/libvirt/vtpm(/.*)?"
+semanage fcontext  -a -s system_u -t virt_image_t "/var/lib/libvirt/vtpmfile(/.*)?"
+semanage fcontext  -a -s system_u -t qemu_exec_t "/usr/libexec/qemu-kvm"
+semanage fcontext  -a -s system_u -t qemu_exec_t "/usr/local/bin/qemu-system-x86_64"
+semanage fcontext  -a -s system_u -t virtd_exec_t "/usr/local/sbin/libvirtd"
+setsebool -P virt_use_execmem 1
 restorecon   -R -v /var/lib/libvirt/vtpm/
+restorecon -R -v /var/lib/libvirt/vtpmfile
+restorecon -R -v /usr/libexec/qemu-kvm
+restorecon -R -v /usr/local/bin/qemu-system-x86_64
+
 echo '----------------------------------'
 
